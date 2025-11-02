55e1410ca6f8a8d2ef8406043daf8b86b583848d
anhtuan021 <doantrungminh20@gmail.com>
2025-11-02 15:29:27 +0700

    commit

---
+++ server/node-build.ts
@@ -0,0 +1,12 @@
+// server/node-build.ts
+import express from "express";
+import { createServer as createViteServer } from "vite";
+
+async function startServer() {
+  const app = express();
+  const vite = await createViteServer({ server: { middlewareMode: true } });
+  app.use(vite.middlewares);
+  app.listen(5173, () => console.log("Server running on http://localhost:5173"));
+}
+
+startServer();
